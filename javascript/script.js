5b48ba86a764acedf85dba6f537e3f5e85609969
Deltafortal <aldolucchetta21@gmail.com>
2023-05-25 15:30:40 +0200

    Added Javascript

--- javascript/script.js
+++ javascript/script.js
@@ -1,1 +1,26 @@
 console.log('JS OK')
+
+// Definizione variabili
+
+let nome;
+let cognome;
+let colore;
+
+// Chiedi di inserire il come e Salvalo
+nome = prompt('Inserisci il tuo nome' , 'Aldo');
+console.log(nome);
+
+// Chiedi di inserire il suo cognome e salvalo
+cognome = prompt('Inserisci il tuo cognome' , 'Lucchetta');
+console.log(cognome);
+
+// Chiedi di inserire il suo colore preferito e salvalo
+colore = prompt('Inserisci il tuo colore preferito' , 'Viola');
+console.log(colore);
+
+// Somma le variabili
+const passw = nome + cognome + colore + '22';
+console.log(passw);
+
+// trova il placehoder nel DOM e inserisci la password generata
+document.getElementById('pwPlaceHolder').innerText = passw;
--- pwgen.md
+++ pwgen.md
@@ -7,7 +7,6 @@
 
 - 01 Definisci una variabile per il nome
 - 02 Definisci una variabile per il cognome
-
 - 03 Definisci una variabile per il colore
 - 04 Chiedi di inserire il nome all'utente
 - 05 Salva il nome nella variabile
@@ -15,6 +14,6 @@
 - 07 Salva il cognome nella variabile
 - 08 Chiedi di inserire il colore all'utente
 - 09 Salva il colore nella variabile
-- 10 Somma il nome, il cognome il colore e aggiungi 21
+- 10 Crea una variabile per la somma del nome, del cognome del colore e aggiungi 21
 - 11 Trova il "pwPlaceholder" nel DOM
 - 12 Inserisci la password generata
